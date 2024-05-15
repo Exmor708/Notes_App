2ae151aa0f3b8aa207652343105e3ef8d18ccd34
Biswajit Pradhan <1901289083@trident.ac.in>
2024-05-15 20:43:01 +0530

    register & login page added

---
+++ src/main/java/dto/Notes.java
@@ -0,0 +1,28 @@
+package dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+@Entity
+@Getter
+@Setter
+
+public class Notes {
+	@Id
+	
+	private int id;
+	
+	private String title;
+	
+	private String description;
+	
+	@ManyToOne
+	@JoinColumn
+	
+	private User user;
+
+}
